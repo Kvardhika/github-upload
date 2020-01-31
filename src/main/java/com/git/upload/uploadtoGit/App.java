3350d284882fcf3f27469e5a4911a6f764307bae
Kvardhika <52639123+Kvardhika@users.noreply.github.com>
2020-01-31 13:48:08 -0600

    Committing initial files

---
+++ src/main/java/com/git/upload/uploadtoGit/App.java
@@ -0,0 +1,13 @@
+package com.git.upload.uploadtoGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
